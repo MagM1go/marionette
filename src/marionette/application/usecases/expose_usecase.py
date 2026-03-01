a397b86e98d84c8f685fe88e0967bd927d6ccd94
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-03-01 13:52:53 +0700

    refactor: `ExposeUseCase.expose` returns None instead of `Result`
    Also remove unused arguments

--- src/marionette/application/usecases/expose_usecase.py
+++ src/marionette/application/usecases/expose_usecase.py
@@ -2,11 +2,8 @@ import random
 
 from marionette.application.dto.embed import Embed
 from marionette.application.dto.result import Result
-from marionette.domain.repositories import (
-    IAgencyRepository,
-    ICharacterRepository,
-    ICooldownRepository,
-)
+from marionette.domain.entities.character import Character
+from marionette.domain.repositories import ICooldownRepository
 from marionette.domain.services.rating_service import RatingChangeReason, RatingService
 from marionette.domain.uow import IUnitOfWork
 from marionette.presentation.colors import Color
@@ -15,35 +12,24 @@ from marionette.presentation.colors import Color
 class ExposeCharacterUseCase:
     ONE_DAY: int = 60 * 60 * 24
     EXPOSE_CHANCE: tuple[float, float] = (0.2, 0.5)
-    
+
     def __init__(
         self,
         rating_service: RatingService,
-        character_repo: ICharacterRepository,
-        agency_repo: IAgencyRepository,
         uow: IUnitOfWork,
         cooldown_repo: ICooldownRepository,
     ) -> None:
         self.rating_service = rating_service
-        self.character_repo = character_repo
-        self.agency_repo = agency_repo
         self.uow = uow
         self.cooldown_repo = cooldown_repo
 
-    async def expose(self, user_id: int, name: str) -> Result:
-        character = await self.character_repo.get_by_user_id_and_name(
-            name=name, user_id=user_id
-        )
-
-        if not character:
-            return Result()
-
-        cd_key = f"cooldown:{user_id}:{character.id}"
+    async def expose(self, character: Character) -> Result | None:
+        cd_key = f"cooldown:{character.user_id}:{character.id}"
         if await self.cooldown_repo.is_on_cooldown(cd_key):
-            return Result()
+            return None
 
         if not (self.EXPOSE_CHANCE[0] < random.random() < self.EXPOSE_CHANCE[1]):
-            return Result()
+            return None
 
         async with self.uow as uow:
             character_new_rating = self.rating_service.dec_character_rating(
