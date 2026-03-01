d2690ea6184ade0a6db168018d621eb58b86e408
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-03-01 10:38:15 +0700

    5

--- src/marionette/application/usecases/entrance_usecase.py
+++ src/marionette/application/usecases/entrance_usecase.py
@@ -9,11 +9,7 @@ class EntranceUseCase:
     async def execute(
         self, user_id: int, character_name: str, thread_id: int
     ) -> Result:
-        entranced_character = await self.roleplay_service.entrance_timeline(
+        await self.roleplay_service.entrance_timeline(
             user_id=user_id, character_name=character_name, thread_id=thread_id
         )
-        
-        if not entranced_character:
-            return Result(f"У вас нет персонажа с именем **{character_name}**. Выберите персонажа из выпадающего списка")
-            
         return Result(f"Вы успешно присоединились к таймлайну! Добро пожаловать: <#{thread_id}>")
