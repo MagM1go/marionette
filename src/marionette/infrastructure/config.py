e8d1aec2e80857c914ed9fcf78854702f34ef942
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-03-10 21:46:58 +0700

    feat: add RP categories

--- src/marionette/infrastructure/config.py
+++ src/marionette/infrastructure/config.py
@@ -1,9 +1,5 @@
 import os
-from dataclasses import dataclass
-
-from dotenv import load_dotenv
-
-load_dotenv()
+from dataclasses import dataclass, field
 
 
 @dataclass
@@ -13,11 +9,14 @@ class Config:
 
     # Discord
     MAIN_GUILD_ID: int = int(os.environ["MAIN_GUILD_ID"])
+    NEWS_CHANNEL_ID: int = int(os.environ["NEWS_CHANNEL_ID"])
     TABLOID_CHANNEL_ID: int = int(os.environ["TABLOID_CHANNEL_ID"])
     REGISTRATION_CHANNEL_ID: int = int(os.environ["REGISTRATION_CHANNEL_ID"])
-    RP_CATEGORIES: list[int] = [
-        int(category_id) for category_id in os.environ["RP_CATEGORIES"].split(",")
-    ]
+    RP_CATEGORIES: list[int] = field(
+        default_factory=lambda: [
+            int(category_id) for category_id in os.environ["RP_CATEGORIES"].split(",")
+        ]
+    )
 
     # Infrastructure
     DATABASE_URL: str = os.environ["DATABASE_URL"]
