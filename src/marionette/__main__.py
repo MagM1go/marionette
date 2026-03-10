3c34c485a6c10ecfd0818d0837ae930cf99d1f7c
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-03-10 21:40:43 +0700

    refactor: remove deadcode from entrypoint

--- src/marionette/__main__.py
+++ src/marionette/__main__.py
@@ -1,44 +1,20 @@
-import asyncio
-from datetime import datetime
-
 import crescent
 import hikari
+import miru
 from dishka import make_async_container
 
-from marionette.domain.entities.character import Character
-from marionette.domain.roles import Roles
 from marionette.infrastructure.config import config
-from marionette.infrastructure.db.manager import DBManager
-from marionette.infrastructure.di.container import CrescentContainer
-from marionette.infrastructure.di.db_provider import ApplicationProvider
-from marionette.infrastructure.repositories.character_repository import (
-    CharacterRepository,
-)
+from marionette.presentation.di.container import CrescentContainer
+from marionette.presentation.di.db_provider import ApplicationProvider
 
 bot = hikari.GatewayBot(token=config.MARIONETTE_TOKEN, intents=hikari.Intents.ALL)
 dishka_container = make_async_container(ApplicationProvider())
+miru_client = miru.Client(bot)
 
 client = crescent.Client(
     bot,
-    model=CrescentContainer(
-        dishka_container,
-    ),
+    model=CrescentContainer(dishka_container, miru_client),
 )
-client.plugins.load_folder("src.marionette.discord.plugins")
+client.plugins.load_folder("src.marionette.presentation.discord.plugins")
 
-
-'''async def main():
-    db = DBManager(config.DATABASE_URL)
-
-    async with db.get_session() as session:
-        repo = CharacterRepository(session)
-        await repo.save(
-            name="Аяна",
-            user_id=598387707311554570,
-            role=Roles.IDOL,
-            birthday=datetime.today(),
-            home_channel_id=1473720774605930624
-        )
-'''
-# asyncio.run(main())
 bot.run()
