8d3771bc4a6713fad1d1c0329555a8c947bc0a25
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-03-10 21:41:34 +0700

    refactor: move domain repositories to application and set it as protocol

--- src/marionette/application/protocols.py
+++ src/marionette/application/protocols.py
@@ -1,15 +1,14 @@
-from abc import ABC, abstractmethod
 from collections.abc import Sequence
 from datetime import datetime
+from typing import Protocol
 
 from marionette.domain.entities.agency import Agency
 from marionette.domain.entities.character import Character
 from marionette.domain.roles import Roles
 
 
-class ICharacterRepository(ABC):
-    @abstractmethod
-    async def save(
+class ICharacterRepository(Protocol):
+    def create(
         self,
         user_id: int,
         name: str,
@@ -17,7 +16,7 @@ class ICharacterRepository(ABC):
         birthday: datetime,
         home_channel_id: int,
     ) -> Character | None:
-        """Создаёт и сохраняет нового персонажа.
+        """Создаёт нового персонажа.
 
         Args:
             user_id: Discord ID пользователя.
@@ -29,15 +28,10 @@ class ICharacterRepository(ABC):
             Созданный персонаж или None, если персонаж с таким именем уже существует у пользователя.
         """
 
-    @abstractmethod
-    async def update(self) -> None:
-        """Фиксирует изменения текущей сессии в базе данных."""
-
-    @abstractmethod
     async def get_all(self) -> Sequence[Character]:
         """Возвращает всех персонажей всех игроков."""
+        ...
 
-    @abstractmethod
     async def set_active(self, user_id: int, name: str, is_active: bool) -> None:
         """Устанавливает активного персонажа игрока.
 
@@ -51,7 +45,6 @@ class ICharacterRepository(ABC):
             is_active: True - установить как активного, False - убирает активного персонажа.
         """
 
-    @abstractmethod
     async def set_location(self, character: Character, channel_id: int | None) -> None:
         """Вход/выход из временной линии (РП-канала)
 
@@ -60,7 +53,6 @@ class ICharacterRepository(ABC):
             channel_id: Discord ID канала
         """
 
-    @abstractmethod
     async def get_by_user_id_and_name(
         self, user_id: int, name: str
     ) -> Character | None:
@@ -74,7 +66,6 @@ class ICharacterRepository(ABC):
             Персонаж или None, если не найден.
         """
 
-    @abstractmethod
     async def get_all_characters_by_user_id(self, user_id: int) -> Sequence[Character]:
         """Возвращает всех персонажей пользователя.
 
@@ -84,8 +75,8 @@ class ICharacterRepository(ABC):
         Returns:
             Список персонажей. Пустой список если персонажей нет.
         """
+        ...
 
-    @abstractmethod
     async def get_by_character_id(self, character_id: int) -> Character | None:
         """Возвращает персонажа по его ID.
 
@@ -95,39 +86,38 @@ class ICharacterRepository(ABC):
         Returns:
             Персонаж или None, если не найден.
         """
-        
-    @abstractmethod
-    async def get_entranced_character_by_user_id(self, user_id: int) -> Character | None:
+
+    async def get_entranced_character_by_user_id(
+        self, user_id: int
+    ) -> Character | None:
         """Возвращает канал, в котором присутствует активный персонаж
-        
+
         Args:
             user_id: Discord ID пользователя.
-        
+
         Returns:
             Персонаж или None, если не найден.
         """
 
-    @abstractmethod
-    async def delete_by_name_and_user_id(self, name: str, user_id: int) -> bool:
-        """Удаляет персонажа пользователя по имени.
+    async def delete(self, character: Character) -> None:
+        """Удаляет персонажа.
 
         Args:
-            name: Имя персонажа.
-            user_id: Discord ID пользователя.
+            character: Персонаж игрока
 
         Returns:
             True, если персонаж удалён и False, если не найден.
         """
+        ...
 
 
-class IAgencyRepository(ABC):
-    @abstractmethod
-    async def save(
+class IAgencyRepository(Protocol):
+    def create(
         self,
         owner_id: int,
         name: str,
     ) -> Agency | None:
-        """Создаёт и сохраняет новое агентство.
+        """Создаёт новое агентство.
 
         Args:
             owner_id: Discord ID владельца (директора) агентства.
@@ -137,15 +127,10 @@ class IAgencyRepository(ABC):
             Созданное агентство или None, если агентство с таким именем уже существует.
         """
 
-    @abstractmethod
-    async def update(self) -> None:
-        """Фиксирует изменения текущей сессии в базе данных."""
-
-    @abstractmethod
     async def get_all(self) -> Sequence[Agency]:
         """Возвращает все агентства."""
+        ...
 
-    @abstractmethod
     async def get_agency_by_id(self, id: int) -> Agency | None:
         """Возвращает агентство по его ID.
 
@@ -157,9 +142,7 @@ class IAgencyRepository(ABC):
         """
 
 
-class ICooldownRepository(ABC):
-    @abstractmethod
+class ICooldownRepository(Protocol):
     async def is_on_cooldown(self, key: str) -> bool: ...
 
-    @abstractmethod
     async def set_cooldown(self, key: str, seconds: int) -> None: ...
