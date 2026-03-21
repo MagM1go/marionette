c1645efe7fb9087125a53f49eba6a6ff0b6215cc
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-03-21 22:16:18 +0700

    feat: move hardcoded ints to config

--- src/marionette/presentation/di/db_provider.py
+++ src/marionette/presentation/di/db_provider.py
@@ -12,30 +12,34 @@ from marionette.application.protocols import (
     IAgencyRepository,
     ICharacterRepository,
     ICooldownRepository,
+    IOnboardingRepository,
 )
 from marionette.application.usecases.entrance_usecase import EntranceUseCase
 from marionette.application.usecases.exit_usecase import ExitUseCase
+from marionette.application.usecases.onboarding_usecase import OnboardingUseCase
 from marionette.application.usecases.paparazzi_usecase import PaparazziUseCase
 from marionette.application.usecases.season_reset_usecase import SeasonResetUseCase
 from marionette.domain.services.rating_service import RatingService
+from marionette.infrastructure.cache.redis import RedisManager
 from marionette.infrastructure.config import config
-from marionette.infrastructure.db.cache.redis import RedisManager
 from marionette.infrastructure.repositories.agency_repository import AgencyRepository
 from marionette.infrastructure.repositories.character_repository import (
     CharacterRepository,
+)
+from marionette.infrastructure.repositories.onboarding_repository import (
+    OnboardingRepository,
 )
 from marionette.infrastructure.repositories.redis_repository import CooldownRepository
 
 
 class ApplicationProvider(Provider):
     @provide(scope=Scope.APP)
-    async def engine(self) -> AsyncGenerator[AsyncEngine, None]:
+    async def engine(self) -> AsyncGenerator[AsyncEngine]:
         engine = create_async_engine(
             url=config.DATABASE_URL,
-            echo=False,
             pool_pre_ping=True,
-            pool_size=10,
-            max_overflow=20,
+            pool_size=config.DATABASE_POOL_SIZE,
+            max_overflow=config.DATABASE_MAX_OVERFLOW,
         )
         yield engine
         await engine.dispose()
@@ -51,7 +55,7 @@ class ApplicationProvider(Provider):
     @provide(scope=Scope.REQUEST)
     async def session(
         self, factory: async_sessionmaker[AsyncSession]
-    ) -> AsyncGenerator[AsyncSession, None]:
+    ) -> AsyncGenerator[AsyncSession]:
         async with factory() as session:
             try:
                 yield session
@@ -61,7 +65,7 @@ class ApplicationProvider(Provider):
                 raise
 
     @provide(scope=Scope.APP)
-    async def redis_manager(self) -> AsyncGenerator[RedisManager, None]:
+    async def redis_manager(self) -> AsyncGenerator[RedisManager]:
         manager = RedisManager(config.REDIS_URL)
         yield manager
         await manager.dispose()
@@ -79,21 +83,30 @@ class ApplicationProvider(Provider):
         return AgencyRepository(session)
 
     @provide(scope=Scope.REQUEST)
+    def onboarding_repository(self, session: AsyncSession) -> IOnboardingRepository:
+        return OnboardingRepository(session)
+
+    @provide(scope=Scope.REQUEST)
     def rating_service(self) -> RatingService:
         return RatingService()
 
     @provide(scope=Scope.REQUEST)
-    def expose_usecase(
+    def paparazzi_usecase(
         self,
         rating_service: RatingService,
-        session: AsyncSession,
         cooldown_repo: ICooldownRepository,
     ) -> PaparazziUseCase:
         return PaparazziUseCase(
             rating_service=rating_service,
-            session=session,
             cooldown_repo=cooldown_repo,
         )
+
+    @provide(scope=Scope.REQUEST)
+    def onboarding_usecase(
+        self,
+        onboarding_repo: IOnboardingRepository,
+    ) -> OnboardingUseCase:
+        return OnboardingUseCase(repository=onboarding_repo)
 
     @provide(scope=Scope.REQUEST)
     def entrance_usecase(self, character_repo: ICharacterRepository) -> EntranceUseCase:
