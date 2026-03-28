6ef46106cb30f34e00fd2859776248d0de3598e0
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-03-29 03:20:27 +0700

    fix: start_role_id not useless okay, return

--- src/marionette/infrastructure/config.py
+++ src/marionette/infrastructure/config.py
@@ -29,6 +29,7 @@ class Config:
 
     # Discord Roles
     UNVERIFIED_ROLE_ID: int = int(os.environ["UNVERIFIED_ROLE_ID"])
+    START_ROLE_ID: int = int(os.environ["START_ROLE_ID"])
     UNREGISTERED_ROLE_ID: int = int(os.environ["UNREGISTERED_ROLE_ID"])
 
     # Infrastructure
