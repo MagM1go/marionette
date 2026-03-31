95b210dc1e9fb27eb006cb14fe1411110015c8a5
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-03-31 23:17:16 +0700

    refactor: move `nox` dependency to `dev` dependencies

--- noxfile.py
+++ noxfile.py
@@ -2,11 +2,13 @@ import sys
 
 import nox
 
+LINT_PATHS: tuple[str, ...] = ("src", "tests", "noxfile.py")
+
 
 @nox.session(venv_backend="none")
 def lint(session: nox.Session) -> None:
-    session.run(sys.executable, "-m", "mypy", ".")
-    session.run(sys.executable, "-m", "ruff", "check", ".")
+    session.run(sys.executable, "-m", "mypy", *LINT_PATHS)
+    session.run(sys.executable, "-m", "ruff", "check", *LINT_PATHS)
 
 
 @nox.session(venv_backend="none")
