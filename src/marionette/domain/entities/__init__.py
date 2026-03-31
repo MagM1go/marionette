cf4c7dfe1ca14f18a40312cc5112a303d559d28a
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-03-31 23:22:27 +0700

    refactor: remove useless code from __init__

--- src/marionette/domain/entities/__init__.py
+++
@@ -1,7 +0,0 @@
-from marionette.domain.entities.onboarding import (
-    OnboardingEvent,
-    OnboardingRoleGrant,
-    OnboardingState,
-)
-
-__all__ = ["OnboardingEvent", "OnboardingRoleGrant", "OnboardingState"]
