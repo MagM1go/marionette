eee6a7ba9adb638690eec0edc23a9e10a183918e
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-03-31 23:25:38 +0700

    refactor: build_discord_client -> build_discord_bot

--- src/marionette/main.py
+++ src/marionette/main.py
@@ -1,6 +1,6 @@
-from marionette.presentation.discord.bootstrap import build_discord_client
+from marionette.bootstrap.app import build_discord_bot
 
 
 def main() -> None:
-    bot = build_discord_client()
+    bot = build_discord_bot(plugins_path="src.marionette.presentation.discord.plugins")
     bot.run()
