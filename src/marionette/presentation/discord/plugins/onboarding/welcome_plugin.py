de1be4e47cc6f0d08c119850ed4dbbff35cf3022
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-04-12 06:14:42 +0700

    feat(onboarding): welcome plugin enhance

--- src/marionette/presentation/discord/plugins/onboarding/welcome_plugin.py
+++ src/marionette/presentation/discord/plugins/onboarding/welcome_plugin.py
@@ -2,11 +2,12 @@ import crescent
 import hikari
 
 from marionette.application.protocols import UserId
+from marionette.application.usecases.onboarding_reset_usecase import OnboardingResetUseCase
 from marionette.application.usecases.onboarding_usecase import OnboardingUseCase
+from marionette.bootstrap.config import config
 from marionette.bootstrap.di.container import CrescentContainer
 from marionette.bootstrap.di.inject import Inject, inject
-from marionette.domain.entities.onboarding import OnboardingStep
-from marionette.presentation.discord.plugins.onboarding.steps import ID_TO_STEP
+from marionette.presentation.discord.ui.onboarding import onboarding_registry
 
 plugin = crescent.Plugin[hikari.GatewayBot, CrescentContainer]()
 inject_plugin = inject(lambda: plugin.model.dishka())
@@ -19,7 +20,21 @@ async def on_user_arrive(
     event: hikari.MemberCreateEvent, usecase: Inject[OnboardingUseCase]
 ) -> None:
     user_id = UserId(event.user_id)
-    await usecase.move_to(event.guild_id, user_id, OnboardingStep.WELCOME)
+    await event.app.rest.add_role_to_member(
+        guild=config.discord.main_guild_id,
+        user=event.user_id,
+        role=config.discord.unverified_role_id,
+    )
+    await usecase.start(event.guild_id, user_id)
+
+
+@plugin.include
+@crescent.event
+@inject_plugin
+async def on_user_leave(
+    event: hikari.MemberDeleteEvent, usecase: Inject[OnboardingResetUseCase]
+) -> None:
+    await usecase.reset(UserId(event.user_id))
 
 
 @plugin.include
@@ -31,9 +46,12 @@ async def on_interaction_click(
     if event.interaction.guild_id is None:
         return
 
-    if (custom_id := event.interaction.custom_id) not in ID_TO_STEP:
+    step = onboarding_registry.get_target_step(event.interaction.custom_id)
+    if step is None:
         return
 
     await usecase.move_to(
-        event.interaction.guild_id, UserId(event.interaction.user.id), ID_TO_STEP[custom_id]
+        event.interaction.guild_id,
+        UserId(event.interaction.user.id),
+        step,
     )
