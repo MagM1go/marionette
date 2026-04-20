06602d191aafcb424e07aae7ebc81b6d31a3bb82
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-04-20 19:32:01 +0700

    refactor: change way to extract text from assets

--- src/marionette/presentation/discord/presenters/onboarding/hello_presenter.py
+++ src/marionette/presentation/discord/presenters/onboarding/hello_presenter.py
@@ -1,13 +1,9 @@
-from pathlib import Path
+from importlib import resources
 
-# TODO: над будет придумать способ попадать в корень получше, иначе это не очень гибко
-_ASSETS_DIR = Path(__file__).resolve().parents[6] / "assets"
-
-with open(f"{_ASSETS_DIR}/text/hello.txt") as file:
-    _text = file.read()
+HELLO_TEXT = resources.files("marionette.assets.text").joinpath("hello.txt").read_text(encoding="utf8")
 
 
 class HelloPresenter:
     @staticmethod
     def present() -> str:
-        return _text
+        return HELLO_TEXT
