5e7f01a5b0b452169328be24e18f5682dc5edabb
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-05-16 10:04:04 +0700

    feat(Vote): add check character is applied

--- src/marionette/domain/policies/vote_policy.py
+++ src/marionette/domain/policies/vote_policy.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from marionette.domain.entities.character import Character
 from marionette.domain.entities.vote import Vote
 from marionette.domain.exceptions import CharacterNotFound
+from marionette.domain.statuses import CharacterStatus
 
 
 class VotePolicy:
@@ -11,6 +12,11 @@ class VotePolicy:
     @staticmethod
     def ensure_character_exists(character: Character | None, expected_name: str) -> None:
         if character is None:
+            raise CharacterNotFound(name=expected_name)
+
+    @staticmethod
+    def ensure_character_is_applied(character: Character, expected_name: str) -> None:
+        if character.status in (CharacterStatus.MODERATION, CharacterStatus.ABANDONED):
             raise CharacterNotFound(name=expected_name)
 
     @staticmethod
