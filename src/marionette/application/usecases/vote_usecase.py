364aa6fc7894a6c61fa272db2f42602bb20627aa
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-05-31 00:53:40 +0700

    feat(Vote): `VotePolicy.ensure_character_exists` -> `CharacterPolicy.ensure_character_exists`

--- src/marionette/application/usecases/vote_usecase.py
+++ src/marionette/application/usecases/vote_usecase.py
@@ -4,6 +4,7 @@ from marionette.application.protocols import CharacterRepository, VoteRepository
 from marionette.application.protocols.transaction import Transaction
 from marionette.application.protocols.types import CharacterId, UserId
 from marionette.domain.exceptions import VoteOnCooldown
+from marionette.domain.policies.character_policy import CharacterPolicy
 from marionette.domain.policies.vote_policy import VotePolicy
 from marionette.domain.services.rating_service import RatingChangeReason, RatingService
 
@@ -24,34 +25,22 @@ class VoteUseCase:
     # TODO: Если ты подписчик конкретного персонажа,
     #   ты можешь голосовать обычно, либо "нейтрально" - отметиться, что был, но не давать рейтинга. Но ты не можешь голосовать отрицательно.
     #   Если же ты НЕ подписчик, то можно голосовать отрицательно
-    async def vote_for(
-        self, voted_by: UserId, user_id: UserId, character_name: str, now: datetime
-    ) -> None:
+    async def vote_for(self, voted_by: UserId, user_id: UserId, character_name: str, now: datetime) -> None:
         async with self._transaction:
-            character = await self._character_repository.get_by_user_id_and_name(
-                user_id=user_id, name=character_name
-            )
-            VotePolicy.ensure_character_exists(character, character_name)
+            character = await self._character_repository.get_by_user_id_and_name(user_id=user_id, name=character_name)
+            CharacterPolicy.ensure_character_exists(character, character_name)
 
             assert character is not None
             VotePolicy.ensure_character_is_applied(character, character_name)
 
-            vote = await self._vote_repository.get_vote_by_character_id(
-                character_id=CharacterId(character.id)
-            )
+            vote = await self._vote_repository.get_vote_by_character_id(character_id=CharacterId(character.id))
             if vote is not None and VotePolicy.is_on_cooldown(vote=vote, now=now):
-                raise VoteOnCooldown(
-                    character.name, VotePolicy.cooldown_remaining(vote=vote, now=now)
-                )
+                raise VoteOnCooldown(character.name, VotePolicy.cooldown_remaining(vote=vote, now=now))
 
-            d_rating = self._rating_service.inc_character_rating(
-                rating=5, reason=RatingChangeReason.VOTE, in_agency=character.agency_id is not None
-            )
+            d_rating = self._rating_service.inc_character_rating(rating=5, reason=RatingChangeReason.VOTE, in_agency=character.agency_id is not None)
             character.rating += d_rating
             if vote is None:
-                self._vote_repository.create(
-                    character_id=CharacterId(character.id), vote_time=now, voted_by=voted_by
-                )
+                self._vote_repository.create(character_id=CharacterId(character.id), vote_time=now, voted_by=voted_by)
             else:
                 vote.voted_at = now
                 vote.voted_by = voted_by
